f03d5f450c2407c5e223362f02a6eb32f903d381
ljp <17854302001@163.com>
2019-07-30 13:58:34 +0800

    Initial commit

---
+++ src/main/java/com/ljp/HelloMailAppliaction.java
@@ -0,0 +1,10 @@
+package com.ljp;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HelloMailAppliaction<spring> {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMailAppliaction.class,args);
+    }
+}
